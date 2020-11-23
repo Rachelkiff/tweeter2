08a52c13c716ab5d24b1fcdc676a5b3d381ca285
rachel <rachelkiffiak1@gmail.com>
2020-11-22 19:14:55 -0700

    Started the first request for users.

---
+++ Backend/app.py
@@ -0,0 +1,12 @@
+import mariadb
+from flask import Flask, request, Response
+import json
+import dbcreds
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/api/users", methods =["GET", "POST", "PATCH", "DELETE"])
+def users():
+    

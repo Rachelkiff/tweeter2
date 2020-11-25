c993edd951d0feda7d86c42fa1b82e0b48f9d495
rachel <rachelkiffiak1@gmail.com>
2020-11-24 21:06:51 -0700

    Added the SQL to the push and Patch. Also modified the GET.

--- Backend/app.py
+++ Backend/app.py
@@ -3,10 +3,15 @@ from flask import Flask, request, Response
 import json
 import dbcreds
 from flask_cors import CORS
-
+import result
+import string
 
 app = Flask(__name__)
 CORS(app)
+def generateToken():
+  letters = string.ascli_letters
+  result_str = '',join(random.choice(letters)for i in range (40))
+  return result_str
 @app.route("/api/users", methods =["GET", "POST", "PATCH", "DELETE"])
 def users():
     if request.method == "GET":
@@ -16,8 +21,11 @@ def users():
       try:
         conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM user")
-        users = cursor.fetchall()
+        if userId != "" and userId != None:
+          cursor.execute("SELECT * FROM user WHERE id = ?", [userId,])
+        else:
+          cursor.execute("SELECT * FROM user")
+          users = cursor.fetchall()
       except Exception as error:
         print("Something went wrong(This is LAZY!): ")
         print(error)   
@@ -30,7 +38,7 @@ def users():
         if(users != None):
             return Response(json.dumps(users, default=str), mimetype="application/json", status=200)
         else:
-            return Response("Something went wrong.", mimetype="text/html", status=500) 
+            return Response("UserId does not exist.", mimetype="text/html", status=500) 
     elif request.method == "POST": 
       conn = None
       cursor = None 
@@ -46,6 +54,20 @@ def users():
         cursor.execute("INSERT INTO user(username, password, email, bio, birthday) VALUES (?, ?, ?, ?, ?)", [user_username, user_password, user_email, user_bio, user_birthday,])
         conn.commit()
         rows = cursor.rowcount
+        if(rows == 1):
+          letters = string.ascli_letters
+          result_str = '',join(random.choice(letters)for i in range (40))
+          userId = cursor.lastrowId
+          cursor.execute("INSERT INTO user(login_token, userId) VALUES (?,?)", [result_string, userId,])
+          conn.commit()
+          rows = cursor.rowcount
+          user = {
+            "username": username,
+            "password": password,
+            "email": email,
+            "bio": bio,
+            "birthday": birthday
+          }          
       except Exception as error:
         print("Something went wrong(This is LAZY!): ")
         print(error)   
@@ -62,18 +84,32 @@ def users():
     elif request.method == "PATCH":
       conn = None
       cursor = None 
-      login_token = request.json.get("login_token")
+      user_username = request.json.get("username")
+      user_password = request.json.get("password")
+      user_email = request.json.get("email")
       user_bio = request.json.get("bio")
+      user_birthday = request.json.get("birthday")
+      user_loginToken = request.json.get("loginToken")
       rows = None
       try:
         conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
         cursor = conn.cursor()
-        if login_token != "" and login_token != None:
-           cursor.execute("UPDATE user SET login_token=? WHERE id=?", [login_token, user_id,])
+        cursor.execute("SELECT userId FROM user_session WHERE loginToken=?", [user_loginToken,])
+        user = cursor.fetchone()
+        if user_username != "" and user_username != None:
+           cursor.execute("UPDATE user SET username=? WHERE userId=?", [user_username, user[0],])
+        if user_password != "" and user_password != None:
+            cursor.execute("UPDATE user SET password=? WHERE userId=?", [user_password, user[0],])
+        if user_email != "" and user_email != None:
+            cursor.execute("UPDATE user SET email=? WHERE userId=?", [user_email, user[0],])
         if user_bio != "" and user_bio != None:
-            cursor.execute("UPDATE user SET bio=? WHERE id=?", [user_bio, user_id,])     
+            cursor.execute("UPDATE user SET bio=? WHERE userId=?", [user_bio, user[0],])    
+        if user_birthday != "" and user_birthday != None:
+            cursor.execute("UPDATE user SET birthday=? WHERE userId=?", [user_birthday, user[0],])
         conn.commit()
         rows = cursor.rowcount
+        cursor.execute
+
       except Exception as error:
         print("Something went wrong(This is LAZY!): ")
         print(error)     
@@ -86,16 +122,16 @@ def users():
        if(rows == 1):
           return Response("Updated Success!", mimetype="text/html", status=200)
        else:
-          return return Response("There was an error!", mimetype="text/html", status=500)  
+          return Response("There was an error!", mimetype="text/html", status=500)  
     elif request.method == "DELETE":
       conn = None
       cursor = None 
-      user_id = request.json.get("id")
+      userId = request.json.get("id")
       rows = None
       try:
         conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM user WHERE id=?", [user_id,])   
+        cursor.execute("DELETE FROM user WHERE id=?", [userId,])   
         conn.commit()
         rows = cursor.rowcount
       except Exception as error:
